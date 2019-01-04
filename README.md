facb1647920691eb395f315b49d729ebf82c6587
Jennifer M <jennifermartinez@Jennifers-MacBook-Pro.local>
2019-01-03 20:06:25 -0800

    Final

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Psychic-Game
-Game
+
+Game Instructions
+
+- The user chooses a random letter on the keyboard from a-z.
+
+- The computer will choose a random letter from a-z.
+
+- If the letter chosen by the computer is the 
+   same as the user, that will be counted as one
+   win.
+
+- If the computer and user chose differing letters,
+   that will be counted as one loss.
+
+- The user starts the game with nine lives.
+
+- Once the lives counter reaches zero, the game
+   will then be over, and will restart.
---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+// Create variables for the array, wins, losses, etc...//
+// Global variable //
+var allLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Local variables //
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
+
+// Create the COMPUTERS random guess //
+var compGuess = allLetters[Math.floor(Math.random() * allLetters.length)]; 
+console.log("Computer Guess: " + compGuess);
+
+// Create the USERS guess //
+document.onkeyup = function(event) {
+   
+var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+console.log("userguess", userGuess);
+
+// USER guess versus COMPUTER guess //
+if (guessesSoFar.indexOf(userGuess) <= 0 && allLetters.indexOf(userGuess) >= 0) {
+	guessesSoFar[guessesSoFar.length]=userGuess;
+    guessesLeft--;
+}
+
+// USER guess and COMPUTER guess are the same //
+if (compGuess == userGuess) {
+    wins++;
+    guessesLeft = 9;     
+    guessesSoFar = [];
+    compGuess = allLetters[Math.floor(Math.random() * allLetters.length)]; 
+    console.log("Computer Guess: " + compGuess);
+}
+
+// The counter reaches 0, and the game should reset itself. The counter does not reset once the user reaches 9 losses. 
+if (guessesLeft == 0) {
+    losses++;
+    guessesLeft = 9;
+    guessesSoFar = [];
+    compGuess = allLetters[Math.floor(Math.random() * allLetters.length)]; 
+    console.log("Computer Guess: " + compGuess);
+}
+
+// Display stats in browser //
+var html =
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses Left: " + guessesLeft + "</p>" +
+    "<p>Guesses So Far: " + guessesSoFar + "</p>"; 
+
+document.getElementById("psychic game").innerHTML = html;
+
+}
